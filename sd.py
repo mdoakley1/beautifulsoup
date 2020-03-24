b414440cd0c706c14bf29719d03ab5891edb1b33
mdoakley1 <moakley.email@gmail.com>
2020-03-24 01:48:04 -0400

    simple selenium scripts

---
+++ sd.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+
+#This example requires Selenium WebDriver 3.13 or newer
+with webdriver.Firefox() as driver:
+    wait = WebDriverWait(driver, 10)
+    driver.get("https://google.com/")
+    driver.find_element_by_name("q").send_keys("cheese" + Keys.RETURN)
+    first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "h3>div")))
+    print(first_result.get_attribute("textContent"))
+    input()
+#    time.sleep(30.0)
+  
